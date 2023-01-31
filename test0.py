ce8bdf1435983b59d7402e6fded15b1e31271180
preethamy99 <98869787+preethamy99@users.noreply.github.com>
2023-01-31 17:26:33 +0530

    Update test0.py

--- test0.py
+++ test0.py
@@ -12,3 +12,17 @@ w_df[].unique()
 w_df[].nunique() or w_df.nunique()
 w_df.count
 w_df.info()
+# visulizing the data
+fig = plt.scatter(w_df['Temp_C'],w_df['Rel Hum_%'])
+fig
+# pressure distribution 
+sns.displot(w_df['Press_kPa'], kde = True)
+# distribution of columns
+for i in w_df.column:
+  sns.displot(w_df[i], kde = True)
+#filter data
+w_df[w_df['Temp_C'] < 0]
+# filter based on different conditions
+w_df[(w_df['Weather'] == 'snow') & (w_df['Wind Speed_km/h'] > 40)]
+#checking is there ny null vlues in the data 
+w_df.isnull().sum()
